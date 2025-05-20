9b0562635b52046609172f1cc85e5cb4bd119e06
anas0022 <anasfamilyman@gmail.com>
2025-05-20 10:32:10 +0530

    up

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const servicesLink = document.querySelector('.services-link');
+    const servicesList = document.querySelector('.service-list');
+
+    servicesLink.addEventListener('mouseover', function(e) {
+     
+        servicesList.style.opacity = '1';
+        /* servicesList.style.zIndex = '400000000'; */
+        servicesList.style.display = 'flex';
+  
+    });
+    servicesLink.addEventListener('mouseout', function(e) {
+        servicesList.style.opacity = '0';
+    });
+    servicesList.addEventListener('mouseover', function(e) {
+        servicesList.style.opacity = '1';
+       /*  servicesList.style.zIndex = '400000000'; */
+       servicesList.style.display = 'flex';
+    });
+    servicesList.addEventListener('mouseout', function(e) {
+        servicesList.style.opacity = '0';
+        servicesList.style.zIndex = '0';
+        servicesList.style.display = 'none';
+    });
+   /*  const servicesLink2 = document.querySelector('.services-link2');
+    const servicesList2 = document.querySelector('.services-list2');
+   servicesLink2.addEventListener('click', function(e) {
+    servicesList2.classList.toggle('active');
+   }); */
+    
+    
+});
